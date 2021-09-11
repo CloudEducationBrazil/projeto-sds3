2441ac96902682a812575cb8c15dddca13e992a2
Heleno <helenocardosofilho@gmail.com>
2021-09-11 18:17:57 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Chart from 'react-apexcharts';
 import { BASE_URL } from 'utils/requests';
 import { SaleSum } from 'types/sale';
+import { useState, useEffect } from 'react';
 
 type ChartData = { labels: string[]; series: number[] };
 
@@ -11,19 +12,38 @@ const DonutChart = () => {
   //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
   //  };
 
-  // FORMA ERRADA
-  let chartData: ChartData = { labels: [], series: [] };
+  // useState = manter estado no compenente
+  // useEffect = executar algo na instanciacao ou destruicap do component, obesrvar estado
 
   // FORMA ERRADA
-  axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
-    const data = response.data as SaleSum[];
-    const myLabels = data.map((x) => x.sellerName);
-    const mySeries = data.map((x) => x.sum);
+  //let chartData: ChartData = { labels: [], series: [] };
 
-    chartData = { labels: myLabels, series: mySeries };
-
-    console.log(chartData);
+  // FORMA CERTA
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
   });
+
+  // FORMA ERRADA requisicao assincrona
+  //axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+  //  const data = response.data as SaleSum[];
+  //  const myLabels = data.map((x) => x.sellerName);
+  //  const mySeries = data.map((x) => x.sum);
+  // chartData = { labels: myLabels, series: mySeries };
+  //    console.log(chartData);
+  //  });
+
+  // FORMA CERTA requisicao assincrona
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+
+      setChartData({ labels: myLabels, series: mySeries });
+      //console.log(chartData);
+    });
+  }, []);
 
   const options = {
     legend: {
