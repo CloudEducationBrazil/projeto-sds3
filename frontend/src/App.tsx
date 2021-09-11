2f0075c5cbca2f8aa1330f7933730dcf91772ef7
helenocardoso <helenocardosofilho@gmail.com>
2021-09-11 11:45:05 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,4 @@
 //import React from 'react'; -- a partir da versão 17 do react não precisa do import
-//import logo from './logo.svg';
-//import './App.css';
-
 import Routes from 'Routes';
 
 function App() {
